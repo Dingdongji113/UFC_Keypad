a25787fea0a8ddf0eea51ee4bebb7a2114a7dc6d
叮咚鸡 <a15562676480@gmail.com>
2026-07-10 00:22:29 +0800

    feat: install split LAND CV setup patch

--- main.py
+++ main.py
@@ -84,11 +84,13 @@ def main():
         from ufc.windowing import patch_settings_window_apply_screen
         from ufc.cold_start import patch_cold_start
         from ufc.cold_direct_entry import install_cold_direct_entry
+        from ufc.cold_setup_split import install_split_land_cv_setup
         install_ifei_rpm_fallback()
         install_realtime_rpm_callbacks()
         patch_settings_window_apply_screen(SettingsWindow)
         patch_cold_start(UFCKeypadWindow)
         install_cold_direct_entry(UFCKeypadWindow)
+        install_split_land_cv_setup(UFCKeypadWindow)
         _bootstrap_log("startup/ui/windowing/cold_start modules imported")
 
         _bootstrap_log("creating QApplication")
