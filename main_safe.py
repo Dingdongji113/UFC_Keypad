ecd0db8b3b80d40429f00aa753724acb9c212f13
叮咚鸡 <a15562676480@gmail.com>
2026-07-12 21:24:47 +0800

    fix: install RPM guard in safe mode

--- main_safe.py
+++ main_safe.py
@@ -113,6 +113,7 @@ def main():
         from ufc.hmd_osb_timing import install_hmd_osb_timing_fix
         from ufc.radar_ins_steps import install_radar_ins_step_split
         from ufc.manual_setup_auto import install_manual_setup_automation
+        from ufc.startup_rpm_guard import install_startup_rpm_guard
         from ufc.cold_prompt_polish import install_cold_prompt_polish
         patch_settings_window_apply_screen(SettingsWindow)
         patch_cold_start(UFCKeypadWindow)
@@ -124,6 +125,7 @@ def main():
         install_hmd_osb_timing_fix(UFCKeypadWindow)
         install_radar_ins_step_split(UFCKeypadWindow)
         install_manual_setup_automation(UFCKeypadWindow)
+        install_startup_rpm_guard(UFCKeypadWindow)
         install_cold_prompt_polish(UFCKeypadWindow)
 
         app = QApplication(sys.argv)
